a9b48ef2b46ea168fb928f28ea07b89b424d75e0
Alexander Alcázar <aalcazarprogramacion@gmail.com>
2025-12-12 21:03:27 +0100

    Modelos y primera vista funcionando

--- tareas_dwes/tareas/urls.py
+++ tareas_dwes/tareas/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import detalle_tarea
+from .views import listar_usuarios
 
 
 
 urlpatterns = [
-    path('<uuid:pk>/', detalle_tarea.as_view(), name='detalle_tarea'),
+  #  path('<uuid:pk>/', detalle_tarea.as_view(), name='detalle_tarea'),
+  path('usuarios/', listar_usuarios, name='listar_usuarios'),
 ]
--- tareas_dwes/tareas/views.py
+++ tareas_dwes/tareas/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Tarea
+from .models import Tarea, Usuario
 
 
 
 # Create your views here.
-class detalle_tarea(DetailView):
-    model = Tarea
-    template_name = "tareas/detalle_tarea.html"
-    context_object_name = "tarea"
+
+   #  class detalle_tarea(DetailView):
+    #     model = Tarea
+    #     template_name = "tareas/detalle_tarea.html"
+    #     context_object_name = "tarea"
+
+# Vista listar usuarios
+def listar_usuarios(request):
+    alumnos = Usuario.objects.filter(rol='alumno')
+    profesores = Usuario.objects.filter(rol='profesor')
+
+    return render(request, 'usuarios/listar_usuarios.html', {
+        'alumnos': alumnos,
+        'profesores': profesores
+    })
